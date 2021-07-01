055b2dff2a8f5d9a34cb3604951536b80d0ff77f
Sparkmod <rafadagher@gmail.com>
2021-06-30 22:12:56 -0400

    Feat: Feature #1.2 (#1)
    ## Core
    
    ### Extensions
    - CollectionExtensions: Generic extensions for Collection Types.
    
    ### Cryptography
    - Cryptography: RSA and MD5 generator

---
+++ RDK/Core/Extensions/CollectionExtensions.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RDK.Core.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static string ByteArrayToString(this byte[] bytes)
+        {
+            StringBuilder sBuilder = new (bytes.Length);
+
+            foreach (byte oneByte in bytes)
+            {
+                sBuilder.Append(oneByte.ToString("X2"));
+            }
+
+            return sBuilder.ToString().ToLower();
+        }
+
+        public static string EncodeByteArray(this byte[] bytes)
+        {
+            StringBuilder sBuilder = new(bytes.Length);
+
+            foreach (byte oneByte in bytes)
+            {
+                sBuilder.Append((char)oneByte);
+            }
+
+            return sBuilder.ToString();
+        }
+
+        public static void Move<T>(this List<T> list, T type, int newIndex) where T : ICloneable
+        {
+            list.Remove(type);
+            list.Insert(newIndex, type);
+        }
+
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            HashSet<TKey> knownKeys = new HashSet<TKey>();
+            return source.Where(element => knownKeys.Add(keySelector(element)));
+        }
+
+        public static bool CompareEnumerable<T>(this IEnumerable<T> firstIE, IEnumerable<T> secondIE)
+        {
+            if (firstIE.GetType() != secondIE.GetType())
+            {
+                return false;
+            }
+
+            T[] firstArray = firstIE.ToArray();
+            T[] secondArray = secondIE.ToArray();
+
+            if (firstArray.Length != secondArray.Length)
+            {
+                return false;
+            }
+
+            return !firstArray.Except(secondArray).Any() && !secondArray.Except(firstArray).Any();
+        }
+
+        public static List<T> Clone<T>(this List<T> listToClone) where T : ICloneable => listToClone.Select(type => (T)type.Clone()).ToList();
+
+
+    }
+}
--- RDK/Settings.cs
+++ RDK/Settings.cs
@@ -1,5 +1,6 @@
 ﻿using RDK.Core.IO;
 using RDK.Initialization.Serilog;
+using System.Globalization;
 
 namespace RDK
 {
@@ -7,9 +8,9 @@ namespace RDK
     {
         private static ConsoleBase ConsoleInterface { get; set; } = new ConsoleBase();
 
-        private static readonly string ConsoleTitle = "RDK Library";
-        private static readonly string Version = "0.0.1";
-        private static readonly string[] AsciiLogo =
+        public static readonly string ConsoleTitle = "RDK Library";
+        public static readonly string Version = "0.0.1";
+        public static readonly string[] AsciiLogo =
         {
             " ____    ____    _  __",
             "|  _ \\  |  _ \\  | |/ /",
@@ -18,20 +19,29 @@ namespace RDK
             "|_| \\_\\ |____/  |_|\\_\\",
             "Library use as helper in C# .NET 5.0 projects",
         };
+        public static readonly string Language = "en-US";
 
         // Load basic settings for initialization
         public static void LoadBasic()
         {
             Settings.LoadConsole();
+            Settings.SetCultureInfo();
         }
 
         private static void LoadConsole()
         {
             SerilogConfiguration.Load();
+
             ConsoleInterface.Logo = AsciiLogo;
             ConsoleInterface.DrawAsciiLogo();
             ConsoleInterface.SetTitle($"{ConsoleTitle} - {Version}");
             ConsoleInterface.Start();
+        }
+
+        private static void SetCultureInfo()
+        {
+            // Force Globalization to en-US because we use periods instead of commas for decimals
+            CultureInfo.CurrentCulture = new CultureInfo(Language);
         }
     }
 }
